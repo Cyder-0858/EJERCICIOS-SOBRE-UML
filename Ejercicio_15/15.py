c29af46cc7c5c89ddc55357d0087cee7d77e0d99
Cyder-0858 <shiyu.huang@usp.ceu.es>
2025-10-14 12:12:15 +0000

    Ejercicio_16

---
+++ Ejercicio_15/15.py
@@ -0,0 +1,1 @@
+print("ya esta aplicado")
---
+++ Ejercicio_17/17.py
@@ -0,0 +1,24 @@
+class library:
+    def __init__(self, nombre, dirección, telf, n_empleados):
+        
+
+class floor:
+    def __init__(self, n, capacidad)
+
+class genre:
+    def __init__(self, tema)
+
+class book:
+    def __init__(self, ISBN, titulo)
+        
+class copy:
+    def __init__(self, id, editorial)
+
+class borrow:
+    def __init__(self, f_ini, f_devolucion, duracion)
+        
+class employee:
+    def __init__(self, nombre, id)
+        
+class reader:
+    def __init__(self, nombre, dni)
